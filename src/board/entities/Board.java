2e53b1365812bf034177fd7ff5e2b74b5e807a97
FIRMINOenzo <enzofirmino.camp@gmail.com>
2024-05-14 21:34:37 -0300

    feat: add Board validation and exceptions

--- src/board/entities/Board.java
+++ src/board/entities/Board.java
@@ -1,31 +1,65 @@
 package board.entities;
 
-public class Board {
-    private Integer rows;
-    private Integer columns;
-    private Piece[][] pieces;
+import board.exceptions.BoardException;
 
-    public Board() {
-    }
+public class Board {
+    private final Integer rows;
+    private final Integer columns;
+    private final Piece[][] pieces;
 
     public Board(Integer rows, Integer columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("[Board generation error]: There must be at least 1 row and 1 column to generate a board.");
+        }
+
         this.rows = rows;
         this.columns = columns;
         this.pieces = new Piece[rows][columns];
     }
 
     public Piece piece(Integer row, Integer column) {
+        if (!this.positionExists(row, column)) {
+            throw new BoardException("[Position error]: Position (" + row + ", " + column +  ") does not exists.");
+        }
+
         return this.pieces[row][column];
     }
 
     public Piece piece(Position position) {
+        if (!this.positionExists(position)) {
+            throw new BoardException("[Position error]: Position (" + position + ") does not exists.");
+        }
+
         return this.pieces[position.getRow()][position.getColumn()];
     }
 
+    public Boolean positionExists(Integer row, Integer column) {
+        return (0 < row && row < this.rows)
+                &&
+                (0 < column && column < this.columns);
+    }
+
+    public Boolean positionExists(Position position) {
+        return this.positionExists(position.getRow(), position.getColumn());
+    }
+
+    public Boolean hasAPiece(Position position) {
+        if (!this.positionExists(position)) {
+            throw new BoardException("[Position error]: Position (" + position + ") does not exists.");
+        }
+
+        return this.piece(position.getRow(), position.getColumn()) != null;
+    }
+
     public void placePiece(Piece piece, Position position) {
+        if (this.hasAPiece(position)) {
+            throw new BoardException("[Position error]: Position (" + position + ") already filled.");
+        }
+
         piece.position = position;
         this.pieces[position.getRow()][position.getColumn()] = piece;
     }
+
     public Integer getRows() {
         return this.rows;
     }
