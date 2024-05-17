979f13609569fd70a8c9ed7a95854c54a08f7233
FIRMINOenzo <enzofirmino.camp@gmail.com>
2024-05-17 16:04:06 -0300

    refactor: remove wrong comments

--- src/board/entities/Piece.java
+++ src/board/entities/Piece.java
@@ -1,7 +1,5 @@
 package board.entities;
 
-import java.util.Arrays;
-
 public abstract class Piece {
     protected Position position;
     private final Board board;
@@ -11,22 +9,19 @@ public abstract class Piece {
         this.position = null;
     }
 
-    // ao invés de retornar a matriz inteira, retornar apenas um array
-    // com as posicoes possiveis;
     public abstract boolean[][] possibleMoves();
 
-    // buscar pelo item dentro do array;
     public boolean possibleMove(Position position) {
         return this.possibleMoves()[position.getRow()][position.getColumn()];
     }
 
-    // length de possibleMoves > 0;
     public boolean isThereAnyPossibleMove() {
         boolean[][] possibleMoves = this.possibleMoves();
 
-        for(boolean[] row: possibleMoves) {
-            for(boolean column: row) {
-                if (column) return true;
+        for (boolean[] row : possibleMoves) {
+            for (boolean column : row) {
+                if (column)
+                    return true;
             }
         }
 
